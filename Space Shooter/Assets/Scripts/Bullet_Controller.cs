72b7e9ac56caadd669194c9af79941150d811167
Unknown <wishpixel.games@gmail.com>
2017-12-05 11:42:27 +0000

    Space Shooter - (Part 2) - Shoot Mechanic

---
+++ Space Shooter/Assets/Scripts/Bullet_Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour
+{
+    public float BulletSpeed;
+
+    private void Start()
+    {
+        transform.GetComponent<Rigidbody>().velocity = transform.forward * BulletSpeed;
+    }
+}
